822acb99de4c00ffcbb249980ed21a95493bd429
JErickGM <jerickgm89@gmail.com>
2022-11-14 19:35:08 -0500

    terminado

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        const main = document.querySelector('main')
+        main.style.opacity = 1
+        main.style.filter = 'blur(0px)'
+    }, 1000)
+    
+});
